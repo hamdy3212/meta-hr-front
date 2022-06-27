4434625079389aa255ab9edc63ad559d6fcdf310
hamdy <hamdyyousseif32@gmail.com>
2022-06-27 23:44:34 +0200

    view application update

--- src/Components/Applications/ViewApplication.js
+++ src/Components/Applications/ViewApplication.js
@@ -12,7 +12,7 @@ import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import DialogContentText from "@mui/material/DialogContentText";
 import Avatar from "@mui/material/Avatar";
-import { apiURL } from '../../envvars';
+import { apiURL } from "../../envvars";
 
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
   "& .MuiDialogContent-root": {
@@ -66,6 +66,7 @@ export default function ViewApplication({ selectedApplication }) {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
+        Accept: "application/json",
         Authorization: "Bearer " + localStorage.getItem("token"),
       },
     };
@@ -73,13 +74,13 @@ export default function ViewApplication({ selectedApplication }) {
       `${apiURL}/api/JobApplications/${selectedApplication.id}/cvURL`,
       requestOptions
     );
-    console.log(response);
     if (response.status === 200) {
-      setCV(response.url);
+      const a = await response.json();
+      setCV(a);
     } else {
       alert("something is wrong!");
     }
-  }
+  };
   return (
     <div>
       <Button
@@ -100,26 +101,54 @@ export default function ViewApplication({ selectedApplication }) {
           id="customized-dialog-title"
           onClose={handleClose}
         >
-          Application Title
+          <pre>
+            {selectedApplication.firstName} {selectedApplication.lastName}'s
+            Application{" "}
+          </pre>
         </BootstrapDialogTitle>
         <DialogContent
           dividers
           style={{ display: "flex", flexDirection: "column" }}
         >
           <span>id: {selectedApplication.id}</span>
-          <span>jobPostingId: {selectedApplication.jobPostingId}</span>
-          <span>jobTitle: {selectedApplication.jobTitle}</span>
-          <span>receivedOnUtc :{selectedApplication.receivedOnUtc}</span>
-          <span>stage: {selectedApplication.stage}</span>
-          <span>firstName: {selectedApplication.firstName}</span>
-          <span>lastName: {selectedApplication.lastName}</span>
-          <span>email: {selectedApplication.email}</span>
-          <span>phone: {selectedApplication.phone}</span>
-          <span>linkedInURL: {selectedApplication.linkedInURL}</span>
-          <span>gitHubURL: {selectedApplication.gitHubURL}</span>
-          <span>personalWebsite: {selectedApplication.personalWebsite}</span>
-
-          {CV && <a href={CV} target="_blank">CV</a>}
+          {selectedApplication.jobPostingId && (
+            <span>jobPostingId: {selectedApplication.jobPostingId}</span>
+          )}
+          <span>JobTitle: {selectedApplication.jobTitle}</span>
+          <span>
+            {/* Received: {selectedApplication.receivedOnUtc.split("T")[0]} */}
+          </span>
+          <span>Stage: {selectedApplication.stage}</span>
+          <span>Email: {selectedApplication.email}</span>
+          <span>Phone: {selectedApplication.phone}</span>
+          <ul class="social-icons">
+            {selectedApplication.gitHubURL ? (
+              <li>
+                <a href={selectedApplication.gitHubURL}>
+                  <i class="fa fa-github"></i>
+                </a>
+              </li>
+            ) : null}
+            {selectedApplication.linkedInURL ? (
+              <li>
+                <a href={selectedApplication.linkedInURL}>
+                  <i class="fa fa-linkedin"></i>
+                </a>
+              </li>
+            ) : null}
+            {selectedApplication.personalWebsite ? (
+              <li>
+                <a href={selectedApplication.personalWebsite}>
+                  <i class="fa fa-globe"></i>
+                </a>
+              </li>
+            ) : null}
+          </ul>
+          {CV && (
+            <a href={CV} target="_blank">
+              <i class="fa fa-file-user"></i>
+            </a>
+          )}
         </DialogContent>
         <DialogActions>
           <Button varinat="contained" color="success" onClick={GetCV}>
